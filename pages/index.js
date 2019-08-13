3d290aeccdaf09e4860e77a049067daa3fa642d4
msweeneydev <mail@msweeneydev.com>
2019-08-13 11:34:28 +0100

    add lib

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,33 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { withRouter } from 'next/router';
 import Cookies from 'js-cookie';
+import parseJwt from '../lib/parseJwt';
 
 export default withRouter(({ router }) => {
   if (router.query.token) {
     const { id, token } = router.query;
-    Cookies.set('token', token);
+    Cookies.set('token', token, { expires: 1 });
+    Cookies.set('id', id, { expires: 1 });
     router.push('/');
   }
-  function parseJwt(token) {
-    if (!token) {
-      return;
-    }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
+  if (router.query.logout) {
+    Cookies.remove('token');
+    Cookies.remove('id');
+    router.push('/logout');
   }
-  const [auth, setAuth] = useState([]);
+  const [auth, setAuth] = useState(false);
+  const [profile, setProfile] = useState({});
   useEffect(() => {
-    async function getAuth() {
+    function getAuth() {
       const isAuth = Cookies.get('token');
+      const newProfile = parseJwt(Cookies.get('id'));
       setAuth(!!isAuth);
+      setProfile({ ...newProfile });
     }
     getAuth();
-  }, []);
-  console.log(auth);
+  }, [auth]);
+  console.log('AUTH', auth);
+  console.log('PROFILE', profile);
   return (
     <main>
       <h1>Hello World</h1>
